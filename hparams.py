bb33cbbeec32951a2f2a292ee2e765156a3cf7a5
Ziyaad <xavierbaatjes@yahoo.com>
2023-09-27 16:26:00 +0200

    Update hparams.py

--- hparams.py
+++ hparams.py
@@ -1,6 +1,3 @@
-from text import symbols
-
-
 class hparams:
     seed = 1234
 
@@ -15,7 +12,7 @@ class hparams:
     sch = False
     sch_step = 4000
     epochs = 8000
-    batch_size = 4
+    batch_size = 16
     iters_per_log = 10
     iters_per_ckpt = 100
     weight_decay = 1e-6
