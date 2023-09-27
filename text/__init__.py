fc986ffa00a965366ac6987b446707221a1b7f4b
Ziyaad <xavierbaatjes@yahoo.com>
2023-09-27 15:35:01 +0200

    Update __init__.py

--- text/__init__.py
+++ text/__init__.py
@@ -12,27 +12,6 @@ _letters_ipa = "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜ
 symbols = list(_letters) + list(_letters_ipa) + list(_punctuation)
 
 
-print(symbols)
-
-def text_to_sequence_1(text):
-    text = text.strip()
-    text = english_cleaners(text)
-    text = text.replace(' ', '[SPACE]')
-    output = tokenizer.encode(text)
-    print(output.tokens)
-    print(output.ids)
-    for tok in output.tokens:
-        if tok == '[UNK]':
-            raise Exception(f'Unknown symbol {tok} found')
-    
-    sequence = np.array([output.ids])
-
-    if torch.cuda.is_available():
-        return torch.autograd.Variable(torch.from_numpy(sequence)).cuda().long()
-    else:
-        return torch.autograd.Variable(torch.from_numpy(sequence)).cpu().long()
-
-
 def text_to_sequence_2(text):
     text = text.strip()
     text = english_cleaners(text)
@@ -50,18 +29,3 @@ def text_to_sequence_2(text):
         return torch.autograd.Variable(torch.from_numpy(sequence)).cuda().long()
     else:
         return torch.autograd.Variable(torch.from_numpy(sequence)).cpu().long()
-
-
-def sequence_to_text(tokens):
-    txt = ''.join(tokens)
-    txt = txt.replace('[SPACE]', ' ')
-    print(txt)
-
-
-def _clean_text(text, cleaner_names):
-  for name in cleaner_names:
-    cleaner = getattr(cleaners, name)
-    if not cleaner:
-      raise Exception('Unknown cleaner: %s' % name)
-    text = cleaner(text)
-  return text
