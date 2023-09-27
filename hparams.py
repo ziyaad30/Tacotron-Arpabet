6b6c941eca92094bc7f5f594f3e13c4c99fe9869
Ziyaad <xavierbaatjes@yahoo.com>
2023-09-27 16:36:37 +0200

    Update hparams.py

--- hparams.py
+++ hparams.py
@@ -8,8 +8,8 @@ class hparams:
     n_workers = 4
     epochs = 8000
     batch_size = 16
-    iters_per_log = 10
-    iters_per_ckpt = 100
+    iters_per_log = 500
+    iters_per_ckpt = 1000
     weight_decay = 1e-6
     grad_clip_thresh = 1.0
     eg_text = "Scientists at the CERN laboratory... say they have discovered a new particle."
