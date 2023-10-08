e333962fb5c5cc9e7550a1ddea8f33160e39a88e
Ziyaad <xavierbaatjes@yahoo.com>
2023-10-08 15:54:41 +0200

    Update loss.py

--- tacotron2_model/loss.py
+++ tacotron2_model/loss.py
@@ -46,4 +46,4 @@ class Tacotron2Loss(nn.Module):
         gate_out = gate_out.view(-1, 1)
         mel_loss = nn.MSELoss()(mel_out, mel_target) + nn.MSELoss()(mel_out_postnet, mel_target)
         gate_loss = nn.BCEWithLogitsLoss()(gate_out, gate_target)
-        return mel_loss + gate_loss
+        return mel_loss + gate_loss, (mel_loss.item(), gate_loss.item())
