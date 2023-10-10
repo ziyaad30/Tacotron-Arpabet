6a44e93bd30e9975a69f1062677752453f6b746c
Ziyaad <xavierbaatjes@yahoo.com>
2023-10-10 20:26:13 +0200

    Update hparams.py

--- hparams.py
+++ hparams.py
@@ -9,7 +9,7 @@ class hparams:
     epochs = 8000
     batch_size = 16
     iters_per_log = 10
-    iters_per_ckpt = 100
+    iters_per_ckpt = 1000
     weight_decay = 1e-6
     grad_clip_thresh = 1.0
     eg_text = "There's a way to measure the acute emotional intelligence that has never gone out of style."
