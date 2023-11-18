18a452c022e3ca6f53dd2b0f0a423a289f3e3628
Ziyaad <xavierbaatjes@yahoo.com>
2023-11-18 18:03:27 +0200

    Update checkpoint.py

--- checkpoint.py
+++ checkpoint.py
@@ -4,9 +4,13 @@ import torch
 from typing import Optional
 from unidecode import unidecode
 
-
-
 DEFAULT_ALPHABET = ['_', '-', '!', "'", '(', ')', ',', '.', ':', ';', '?', ' ',
+                    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                    'U', 'V', 'W', 'X', 'Y', 'Z',
+                    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                    'u', 'v', 'w', 'x', 'y', 'z']
+
+NVIDIA_ALPHABET = ['_', '-', '!', "'", '(', ')', ',', '.', ':', ';', '?', ' ',
                    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
                    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
                    '@AA', '@AA0', '@AA1', '@AA2', '@AE', '@AE0', '@AE1', '@AE2', '@AH', '@AH0', '@AH1', '@AH2', '@AO', '@AO0', '@AO1', '@AO2', '@AW',
@@ -17,7 +21,8 @@ DEFAULT_ALPHABET = ['_', '-', '!', "'", '(', ')', ',', '.', ':', ';', '?', ' ',
 
 
 def transfer_symbols_embedding(
-    original_embedding_weight: torch.Tensor, embedding_layer, new_symbols: list, original_symbols: Optional[list] = None
+        original_embedding_weight: torch.Tensor, embedding_layer, new_symbols: list,
+        original_symbols: Optional[list] = None
 ):
     """
     Transfer embedding information from transfer learning model to reduce embedding time.
@@ -35,9 +40,9 @@ def transfer_symbols_embedding(
         list of symbols used by the checkpoint model (defaults to NVIDIA_ALPHABET)
     """
     if original_symbols is None:
-        original_symbols = DEFAULT_ALPHABET
+        original_symbols = NVIDIA_ALPHABET
     assert (
-        len(original_symbols) == original_embedding_weight.shape[0]
+            len(original_symbols) == original_embedding_weight.shape[0]
     ), f"length of original_symbols does not match length of checkpoint model embedding! Got {len(original_symbols)} and {original_embedding_weight.shape[0]}."
 
     weight_tensor = original_embedding_weight.data
@@ -109,10 +114,10 @@ def warm_start_model(checkpoint_path, model, symbols=None, ignore_layers=["embed
     if symbols is None:
         print("WARNING: called warm_start_model with symbols not set. This will be unsupported in the future.")
     if (
-        symbols is not None
-        and old_symbols != symbols
-        and hasattr(model, "embedding")
-        and "embedding.weight" in model_dict
+            symbols is not None
+            and old_symbols != symbols
+            and hasattr(model, "embedding")
+            and "embedding.weight" in model_dict
     ):
         transfer_symbols_embedding(
             checkpoint_dict["state_dict"]["embedding.weight"], model.embedding, symbols, old_symbols
@@ -141,16 +146,17 @@ def get_state_dict(model):
         return model.state_dict()
 
 
-def load_checkpoint(checkpoint_path, model, optimizer, train_loader):
+def load_checkpoint(checkpoint_path, model, optimizer, train_loader, scheduler):
     checkpoint_dict = torch.load(checkpoint_path, map_location="cpu")
     model.load_state_dict(checkpoint_dict["state_dict"])
     optimizer.load_state_dict(checkpoint_dict["optimizer"])
     iteration = checkpoint_dict["iteration"]
+    scheduler.load_state_dict(checkpoint_dict["optimizer"])
     epoch = checkpoint_dict.get("epoch", max(0, int(iteration / len(train_loader))))
-    return model, optimizer, iteration, epoch
- 
- 
-def save_checkpoint(model, optimizer, learning_rate, iteration, symbols, epoch, output_directory):
+    return model, optimizer, iteration, epoch, scheduler
+
+
+def save_checkpoint(model, optimizer, learning_rate, iteration, scheduler, symbols, epoch, output_directory):
     checkpoint_name = "checkpoint_{}".format(iteration)
     output_path = os.path.join(output_directory, checkpoint_name)
     torch.save(
@@ -158,6 +164,7 @@ def save_checkpoint(model, optimizer, learning_rate, iteration, symbols, epoch, 
             "iteration": iteration,
             "state_dict": get_state_dict(model),
             "optimizer": optimizer.state_dict(),
+            'scheduler': scheduler.state_dict(),
             "learning_rate": learning_rate,
             "epoch": epoch,
             "symbols": symbols,
@@ -168,8 +175,9 @@ def save_checkpoint(model, optimizer, learning_rate, iteration, symbols, epoch, 
     return output_path
 
 
-def save_best_checkpoint(model, optimizer, learning_rate, iteration, symbols, epoch, output_directory):
-    checkpoint_name = "tacotron2.pt"
+def save_best_checkpoint(model, iteration, symbols, path):
+    checkpoint_name = os.path.basename(path)
+
     output_path = os.path.join("ckpt", checkpoint_name)
     torch.save(
         {
