7b7fc71be21f0f81a5050767598ea209c6f5534f
Ziyaad <xavierbaatjes@yahoo.com>
2023-11-18 18:04:14 +0200

    Update cleaners.py

--- text/cleaners.py
+++ text/cleaners.py
@@ -3,53 +3,52 @@ import os
 from unidecode import unidecode
 from .numbers import normalize_numbers
 
-
 # Regular expression matching whitespace:
 _whitespace_re = re.compile(r'\s+')
 
 # List of (regular expression, replacement) pairs for abbreviations:
 _abbreviations = [(re.compile('\\b%s\\.' % x[0], re.IGNORECASE), x[1]) for x in [
-  ('mrs', 'misess'),
-  ('mr', 'mister'),
-  ('dr', 'doctor'),
-  ('st', 'saint'),
-  ('co', 'company'),
-  ('jr', 'junior'),
-  ('maj', 'major'),
-  ('gen', 'general'),
-  ('drs', 'doctors'),
-  ('rev', 'reverend'),
-  ('lt', 'lieutenant'),
-  ('hon', 'honorable'),
-  ('sgt', 'sergeant'),
-  ('capt', 'captain'),
-  ('esq', 'esquire'),
-  ('ltd', 'limited'),
-  ('col', 'colonel'),
-  ('ft', 'fort'),
+    ('mrs', 'misses'),
+    ('mr', 'mister'),
+    ('dr', 'doctor'),
+    ('st', 'saint'),
+    ('co', 'company'),
+    ('jr', 'junior'),
+    ('maj', 'major'),
+    ('gen', 'general'),
+    ('drs', 'doctors'),
+    ('rev', 'reverend'),
+    ('lt', 'lieutenant'),
+    ('hon', 'honorable'),
+    ('sgt', 'sergeant'),
+    ('capt', 'captain'),
+    ('esq', 'esquire'),
+    ('ltd', 'limited'),
+    ('col', 'colonel'),
+    ('ft', 'fort'),
 ]]
 
 
 def expand_abbreviations(text):
-  for regex, replacement in _abbreviations:
-    text = re.sub(regex, replacement, text)
-  return text
+    for regex, replacement in _abbreviations:
+        text = re.sub(regex, replacement, text)
+    return text
 
 
 def expand_numbers(text):
-  return normalize_numbers(text)
+    return normalize_numbers(text)
 
 
 def lowercase(text):
-  return text.lower()
+    return text.lower()
 
 
 def collapse_whitespace(text):
-  return re.sub(_whitespace_re, ' ', text)
+    return re.sub(_whitespace_re, ' ', text)
 
 
 def convert_to_ascii(text):
-  return unidecode(text)
+    return unidecode(text)
 
 
 def check_stops(text):
@@ -72,38 +71,45 @@ def check_ellipse(text):
     return text
 
 
-def custom_pronounce(text):
-    text = text.replace("decavalcante", "decavel'kuntay")
-    text = text.replace("simone", "simonie")
-    text = text.replace("apalachin", "apaleikin")
-    
+def check_arpa_stress(text):
+    for test in text.split(" "):
+        if (test.endswith(')')
+                or test.endswith('),')
+                or test.endswith(').')
+                or test.endswith('!,')
+                or test.endswith('?,')
+                or test.endswith('):')
+                or test.endswith(');')):
+            print(test)
+            return text
+    text = expand_numbers(text)
     return text
 
 
 def english_cleaners(text):
-    '''Pipeline for English text, including number and abbreviation expansion.'''
+    """Pipeline for English text, including number and abbreviation expansion."""
     text = text.strip()
     text = convert_to_ascii(text)
     text = lowercase(text)
-    text = custom_pronounce(text)
-    text = expand_numbers(text)
+    text = check_arpa_stress(text)
+    # text = expand_numbers(text)
     text = expand_abbreviations(text)
     text = collapse_whitespace(text)
-    text = text.replace('(', '')
-    text = text.replace(')', '')
-    text = text.replace(':', ' :')
+    text = text.replace('--', '-')
+    # text = text.replace('(', '')
+    # text = text.replace(')', '')
+    text = text.replace(':', ' : ')
     text = text.replace(';', ' ;')
     text = text.replace('"', '')
     text = text.replace('!', ' !')
     text = text.replace('?', ' ?')
     text = text.replace(',', ' ,')
-    text = text.replace('-', ' ')
     text = check_ellipse(text)
     text = check_stops(text)
     return text
 
 
 def english_cleaners_2(text):
-    '''Pipeline for English text, including number and abbreviation expansion.'''
+    """Pipeline for English text, including number and abbreviation expansion."""
     text = english_cleaners(text)
     return text
