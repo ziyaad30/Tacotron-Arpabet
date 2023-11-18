327531ed4a5b108b726964e60e5d9f10ab9b8c02
Ziyaad <xavierbaatjes@yahoo.com>
2023-11-18 18:01:58 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,22 +1,24 @@
 import os
+import random
 import time
-import argparse
+
+import numpy as np
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
-import random
-from utils import get_learning_rate, load_labels_file, train_test_split, calc_avgmax_attention
+
+from checkpoint import warm_start_model, save_checkpoint, load_checkpoint, latest_checkpoint_path, \
+    oldest_checkpoint_path, save_best_checkpoint
 from tacotron2_model import Tacotron2, TextMelCollate, Tacotron2Loss
 from tacotron2_model.utils import process_batch
+from text import symbols
+from utils import load_labels_file, train_test_split, calc_avgmax_attention
 from validate import validate
 from voice_dataset import VoiceDataset
-from text import symbols, text_to_sequence_2, cmudict
-from checkpoint import warm_start_model, save_checkpoint, load_checkpoint, latest_checkpoint_path, oldest_checkpoint_path, save_best_checkpoint
-from logger import Tacotron2Logger
-from hparams import hparams as hps
-from tqdm import tqdm
 
-pbar = tqdm
+
+def to_arr(var):
+    return var.cpu().detach().numpy().astype(np.float32)
 
 
 def load_model(model_path):
@@ -44,190 +46,234 @@ def load_model(model_path):
     return model
 
 
+class Trainer:
+    def __init__(self, total_epochs, metadata_path, audio_directory, batch_size,
+                 iters_per_ckpt, ckpt_to_keep, eg_text, multi_gpu, transfer_learning_path,
+                 m_step, checkpoint_path, learning_rate):
+        self.iteration = None
+        self.epoch = None
+        self.latest_model = None
+        self.logger = None
+        self.isRunning = False
+        self.isSaveable = False
+        self.total_epochs = total_epochs
+        self.batch_size = batch_size
+        self.seed = 1234
+        self.weight_decay = 1e-6
+        self.betas = (0.9, 0.999)
+        self.eps = 1e-6
+        self.train_size = 0.8
+        self.n_workers = 0
+        self.pin_mem = False
+        self.transfer_learning_path = transfer_learning_path
+        self.eg_text = eg_text
+        self.checkpoint_path = checkpoint_path
+        self.log_dir = ''
+        self.grad_clip_thresh = 1.0
+        self.iters_per_log = 5
+        self.iters_per_ckpt = iters_per_ckpt
+        self.ckpt_to_keep = ckpt_to_keep
+        self.metadata_path = metadata_path
+        self.audio_directory = audio_directory
+        self.pretrained = False
+        self.multi_gpu = multi_gpu
+        self.m_step = m_step
+        self.learning_rate = learning_rate
 
-def train(args):
-	epochs = hps.epochs
-	learning_rate = get_learning_rate(hps.batch_size)
-	print(
-		f"Setting batch size to {hps.batch_size}, learning rate to {learning_rate}."
-	)
+        print('Trainer initialized.')
 
-	# Set seed
-	torch.manual_seed(hps.seed)
-	torch.cuda.manual_seed(hps.seed)
-	random.seed(hps.seed)
+    def stop(self):
+        print('Training stopped.')
+        self.isRunning = False
 
-	# Setup GPU
-	torch.backends.cudnn.enabled = True
-	torch.backends.cudnn.benchmark = False
+    def run(self):
+        self.isRunning = True
+        print(f"Setting batch size to {self.batch_size}, learning rate to {self.learning_rate}.")
 
-	# Load model & optimizer
-	print("Loading model...")
-	model = Tacotron2().cuda()
+        # Set seed
+        torch.manual_seed(self.seed)
+        torch.cuda.manual_seed(self.seed)
+        random.seed(self.seed)
 
-	optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=hps.weight_decay)
+        # Setup GPU
+        torch.backends.cudnn.enabled = True
+        torch.backends.cudnn.benchmark = False
 
-	criterion = Tacotron2Loss()
-	print("Loaded model")
+        # Load model & optimizer
+        print("Loading model...")
+        model = Tacotron2().cuda()
 
-	# metadata.csv
-	filepaths_and_text = load_labels_file(args.metadata_path)
-	random.shuffle(filepaths_and_text)
-	train_files, test_files = train_test_split(filepaths_and_text, hps.train_size)
+        optimizer = torch.optim.Adam(model.parameters(), lr=self.learning_rate, betas=self.betas, eps=self.eps,
+                                     weight_decay=self.weight_decay)
+        scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=self.m_step, gamma=0.5)
 
-	trainset = VoiceDataset(train_files, args.audio_directory, hps.cmudict_path)
-	valset = VoiceDataset(test_files, args.audio_directory, hps.cmudict_path)
-	collate_fn = TextMelCollate()
+        criterion = Tacotron2Loss()
+        print("Loaded training models")
 
-	# Data loaders
-	train_loader = DataLoader(
-		trainset, num_workers=hps.n_workers, sampler=None, batch_size=hps.batch_size, pin_memory=hps.pin_mem, collate_fn=collate_fn
-	)
-	val_loader = DataLoader(
-		valset, num_workers=hps.n_workers, sampler=None, batch_size=hps.batch_size, pin_memory=hps.pin_mem, collate_fn=collate_fn
-	)
-	print("Loaded data")
+        # metadata.csv
+        filepaths_and_text = load_labels_file(self.metadata_path)
+        random.shuffle(filepaths_and_text)
+        train_files, test_files = train_test_split(filepaths_and_text, self.train_size)
 
-	# Load checkpoint if one exists
-	iteration = 0
-	epoch_offset = 0
+        trainset = VoiceDataset(train_files, self.audio_directory)
+        valset = VoiceDataset(test_files, self.audio_directory)
+        collate_fn = TextMelCollate()
 
-	if args.resume:
-		latest_model = latest_checkpoint_path(hps.checkpoint_path)
-		if args.transfer_learning_path:
-			print("Ignoring transfer learning as checkpoint already exists")
-		model, optimizer, iteration, epoch_offset = load_checkpoint(str(latest_model), model, optimizer, train_loader)
-		iteration += 1
-		print("Loaded checkpoint '{}' from iteration {}".format(hps.checkpoint_path, iteration))
-	elif args.transfer_learning_path:
-		model = warm_start_model(args.transfer_learning_path, model, symbols)
-		print("Loaded transfer learning model '{}'".format(args.transfer_learning_path))
-	else:
-		print("Generating first checkpoint...")
+        # Data loaders
+        train_loader = DataLoader(
+            trainset, num_workers=self.n_workers, sampler=None, batch_size=self.batch_size, pin_memory=self.pin_mem,
+            collate_fn=collate_fn
+        )
+        val_loader = DataLoader(
+            valset, num_workers=self.n_workers, sampler=None, batch_size=self.batch_size, pin_memory=self.pin_mem,
+            collate_fn=collate_fn
+        )
 
-	if args.pretrained:
-		iteration = 0
-		epoch_offset = 0
+        # Load checkpoint if one exists
+        self.iteration = 0
+        epoch_offset = 0
 
-	# Enable Multi GPU
-	if args.multi_gpu and torch.cuda.device_count() > 1:
-		print(f"Using {torch.cuda.device_count()} GPUs")
-		model = nn.DataParallel(model)
+        try:
+            self.latest_model = latest_checkpoint_path(self.checkpoint_path)
+        except (Exception,):
+            pass
 
-	cmu = cmudict.CMUDict(hps.cmudict_path)
-	alignment_sequence = text_to_sequence_2(hps.eg_text, dictionary=cmu)
+        if self.latest_model is not None:
+            if self.transfer_learning_path:
+                print("Ignoring transfer learning as checkpoint already exists")
 
-	if hps.checkpoint_path != '':
-		if not os.path.isdir(hps.checkpoint_path):
-			os.makedirs(hps.checkpoint_path)
-			os.chmod(hps.checkpoint_path, 0o775)
-    
-	if args.log_dir != '':
-		if not os.path.isdir(args.log_dir):
-			os.makedirs(args.log_dir)
-			os.chmod(args.log_dir, 0o775)
-		logger = Tacotron2Logger(args.log_dir)
-    
-	model.train()
-	validation_losses = []
-    
-	prev_val_loss = 10
-	prev_avgmax_attention = 0
-    
-	for epoch in range(epoch_offset, epochs):
-		# print(f"Progress - {epoch}/{epochs} Epochs")
-		for _, batch in enumerate(pbar(train_loader, desc="Epoch {}".format(epoch), position=0, leave=False)):
-			start = time.perf_counter()
-			
-			for param_group in optimizer.param_groups:
-				param_group["lr"] = learning_rate
+            model, optimizer, self.iteration, epoch_offset, scheduler = load_checkpoint(str(self.latest_model), model,
+                                                                                        optimizer, train_loader,
+                                                                                        scheduler)
+            scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=self.m_step, gamma=0.5,
+                                                             last_epoch=self.iteration)
+            self.iteration += 1
+            print("Loaded checkpoint '{}' from iteration {}".format(str(self.latest_model), self.iteration))
 
-			# Backpropogation
-			model.zero_grad()
-			y, y_pred = process_batch(batch, model)
+        elif self.transfer_learning_path:
+            model = warm_start_model(self.transfer_learning_path, model, symbols)
+            print("Loaded transfer learning model '{}'".format(self.transfer_learning_path))
 
-			loss, items = criterion(y_pred, y)
-			avgmax_attention = calc_avgmax_attention(batch[-1], batch[1], y_pred[-1])
-			reduced_loss = loss
-			loss.backward()
+        else:
+            print("Generating first checkpoint...")
 
-			grad_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), hps.grad_clip_thresh)
-			optimizer.step()
-			
-			duration = time.perf_counter() - start
-			
-			if iteration % hps.iters_per_log == 0:
-				logger.log_training(reduced_loss, grad_norm, learning_rate, iteration)
-				pbar.write('Step: {}, Mel Loss: {:.7f}, Gate Loss: {:.7f}, Grad Norm: {:.7f}, lr: {:.5f} {:.1f}s/it'.format(iteration, items[0], items[1], grad_norm, learning_rate, duration))
+        if self.pretrained:
+            self.iteration = 0
+            epoch_offset = 0
 
-			# Validate & save checkpoint
-			if iteration % hps.iters_per_ckpt == 0:
-				pbar.write("Validating model")
+        # Enable Multi GPU
+        if self.multi_gpu and torch.cuda.device_count() > 1:
+            print(f"Using {torch.cuda.device_count()} GPUs")
+            model = nn.DataParallel(model)
 
-				val_loss, avgmax_attention = validate(model, val_loader, criterion, iteration)
+        model.train()
+        validation_losses = []
 
-				validation_losses.append(val_loss)
-				
-				
-				pbar.write(
-					"Saving model and optimizer state at iteration {} to {}. Validation score = {:.5f}, Attention score = {:.5f}".format(
-						iteration, hps.checkpoint_path, val_loss, avgmax_attention
-					)
-				)
-				logger.log_validation(iteration, val_loss, avgmax_attention)
-				checkpoint_path = save_checkpoint(
-					model,
-					optimizer,
-					learning_rate,
-					iteration,
-					symbols,
-					epoch,
-					hps.checkpoint_path,
-				)
-				if alignment_sequence is not None:
-					pbar.write(f'Evaluating model {checkpoint_path}...')
-					model.eval()
-					mel_outputs, mel_outputs_postnet, _, alignment = load_model(checkpoint_path).inference(alignment_sequence)
-					model.train()
-					logger.sample_train(y_pred, iteration)
-					logger.sample_infer(mel_outputs, mel_outputs_postnet, alignment, iteration)
-					model.train()
-				
-				old_ckpt = oldest_checkpoint_path(hps.checkpoint_path, "checkpoint_[0-9]*", preserved=2)
-				if os.path.exists(old_ckpt):
-					pbar.write(f"Removed {old_ckpt}")
-					os.remove(old_ckpt)
-					
-				pbar.write(f"Current score: {val_loss:.7f} - Previous score: {prev_val_loss:.7f}")
-				
-				if val_loss < prev_val_loss:
-					pbar.write(f'Best -- Validation score: {val_loss}, Attention score: {avgmax_attention}')
-					prev_avgmax_attention = avgmax_attention
-					prev_val_loss = val_loss
-					
-					checkpoint_path = save_best_checkpoint(
-						model,
-						optimizer,
-						learning_rate,
-						iteration,
-						symbols,
-						epoch,
-						hps.checkpoint_path,
-					)
+        for epoch in range(epoch_offset, self.total_epochs + 1):
+            self.epoch = epoch
+            print(self.epoch, self.total_epochs)
+            if not self.isRunning:
+                print('Break while switching epoch...')
+                if self.isSaveable:
+                    save_checkpoint(
+                        model,
+                        optimizer,
+                        self.learning_rate,
+                        self.iteration,
+                        scheduler,
+                        symbols,
+                        self.epoch,
+                        self.checkpoint_path,
+                    )
+                break
 
-			iteration += 1
-    
+            for i, batch in enumerate(train_loader):
+                if not self.isRunning:
+                    print('Break while switching step...')
+                    break
+
+                start = time.perf_counter()
+
+                # Back propagation
+                model.zero_grad()
+                y, y_pred = process_batch(batch, model)
+
+                loss, items = criterion(y_pred, y)
+                avgmax_attention = calc_avgmax_attention(batch[-1], batch[1], y_pred[-1])
+                reduced_loss = loss
+                loss.backward()
+
+                grad_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), self.grad_clip_thresh)
+
+                optimizer.step()
+
+                duration = time.perf_counter() - start
+                lr = scheduler.get_last_lr()[0]
+
+                if self.iteration % self.iters_per_log == 0:
+                    print('Step: {}, Mel Loss: {:.7f}, Gate Loss: {:.7f}, Grad Norm: {:.7f}, Attention score {:.7f}, '
+                          'lr: {:.7f} {:.1f}s/it'
+                          .format(self.iteration, items[0], items[1], grad_norm, avgmax_attention, lr, duration))
+
+                # Validate & save checkpoint
+                if self.iteration % self.iters_per_ckpt == 0:
+                    val_loss, avgmax_attention = validate(model, val_loader, criterion, self.iteration)
+
+                    msg = ("Saving model and optimizer state at iteration {} to {}. Validation score = {:.5f}, "
+                           "Attention score = {:.5f}").format(
+                        self.iteration, self.checkpoint_path, val_loss, avgmax_attention
+                    )
+
+                    print(msg)
+
+                    save_checkpoint(
+                        model,
+                        optimizer,
+                        self.learning_rate,
+                        self.iteration,
+                        scheduler,
+                        symbols,
+                        self.epoch,
+                        self.checkpoint_path,
+                    )
+
+                    self.check_checkpoints()
+
+                    print("=====================================================")
+                self.isSaveable = True
+                self.iteration += 1
+                scheduler.step()
+            epoch += 1
+        if self.isRunning:
+            self.train_complete(model)
+
+    def train_complete(self, model):
+        save_best_checkpoint(
+            model,
+            self.iteration,
+            symbols,
+            self.checkpoint_path
+        )
+        print(f'Training completed with {self.epoch} epochs at {self.iteration}')
+
+    def check_checkpoints(self):
+        old_ckpt = oldest_checkpoint_path(self.checkpoint_path, "checkpoint_[0-9]*", preserved=self.ckpt_to_keep)
+        if os.path.exists(old_ckpt):
+            print(f"Removed {old_ckpt}")
+            os.remove(old_ckpt)
+
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
-	parser.add_argument("-m", "--metadata_path", type=str, default = './LJSPEECH/metadata.csv', help="metadata path")
-	parser.add_argument("-l", "--transfer_learning_path", type=str, default = 'tacotron2_statedict.pt', help="learning path")
-	parser.add_argument("-a", "--audio_directory", type=str, default = './LJSPEECH/wavs', help="directory to audio")
-	parser.add_argument("--log_dir", type=str, default = './logs', help="tensorboard log directory")
-	parser.add_argument('-r', '--resume', action='store_true')
-	parser.add_argument('-p', '--pretrained', action='store_true')
-	parser.add_argument('-multi', '--multi_gpu', action='store_true')
-
-	args = parser.parse_args()
-
-	train(args)
+    trainer = Trainer(2000, 
+                      './dataset/metadata.csv', 
+                      './dataset/wavs',
+                      16, 
+                      500, 
+                      2, 
+                      '', 
+                      False,
+                      './tacotron2_statedict.pt', 
+                      [1000, 2000, 35000],
+                      './checkpoints', 
+                      0.0002)
+    trainer.run()
